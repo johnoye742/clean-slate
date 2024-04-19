738650438d602d8e579e14b0f4fd3b8257881933
John Oye <olugbengajohnoye@gmail.com>
2024-04-19 12:05:36 +0100

    The begining of an end

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+# first of all I want to say, fuck Python
+
 import os
 
 contains = input("File contains: ")
 dir = input("Directory or Folder path: ")
 
-files = os.listdir(dir)
+directoryList = [dir]
+cursor = 0
 
-for f in files:
-    filepath = dir + '/' + f
-    if(os.path.isdir(filepath)):
-            li = os.listdir(filepath)
-            for f1 in li:
-                filepath = dir + '/' + f1
-                if contains in f1:
-                    os.remove(filepath)
-                    print('Deleted '+f1)
+def deleteFiles() :
+    for directory in directoryList:
+        files = os.listdir(directory)
 
-    if contains in f:
-            os.remove(filepath)
-            print('Deleted '+f)
+        for f in files:
+            filepath = dir + '/' + f
+            if(os.path.isdir(filepath)):
+                directoryList.append(filepath)
+                deleteFiles()
+            elif contains in f:
+                os.remove(filepath)
+                print('Deleted '+f)
+
+
+    
+
+print(directoryList)
